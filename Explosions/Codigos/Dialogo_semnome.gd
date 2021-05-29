18901c9038557e09e76814e8d7a62afd9155565e
Vicilius <viniciusmachado19@gmail.com>
2021-05-29 20:20:43 -0300

    be

---
+++ Explosions/Codigos/Dialogo_semnome.gd
@@ -0,0 +1,75 @@
+extends Control
+
+export var dialogPath = ""
+export(float) var textSpeed = 0.05
+
+var dialog
+
+var phraseNum = 0
+var finished = false
+
+func _ready():
+	$Timer.wait_time = textSpeed
+	dialog = getDialog()
+	assert(dialog, "Dialog not found")
+	nextPhrase()
+	
+	
+	
+func _process(delta):
+	$TextureRect/NinePatchRect.visible = finished
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if finished:
+			nextPhrase()
+		else:
+			$TextureRect/Label.visible_characters = len($TextureRect/Label.text)
+			
+	if Input.is_action_just_pressed("accept"):
+		if finished:
+			nextPhrase()
+		else:
+			$TextureRect/Label.visible_characters = len($TextureRect/Label.text)
+			
+
+func getDialog() -> Array:
+	var f = File.new()
+	assert(f.file_exists(dialogPath), "File Path does not exist")
+	
+	f.open(dialogPath, File.READ)
+	var json = f.get_as_text()
+	
+	var output = parse_json(json)
+	
+	if typeof(output) == TYPE_ARRAY:
+		return output
+	else:
+		return []
+		
+
+func nextPhrase() -> void:
+	if phraseNum >= len(dialog):
+		queue_free()
+		return
+		
+	finished = false
+	
+	
+	$TextureRect/Label.text = dialog[phraseNum]["Text"]
+	
+	$TextureRect/Label.visible_characters = 0
+	
+	
+	
+	
+	
+	while $TextureRect/Label.visible_characters < len($TextureRect/Label.text):
+		$TextureRect/Label.visible_characters += 1
+		
+		$Timer.start()
+		yield($Timer, "timeout")
+	
+	finished = true
+	phraseNum += 1
+	return
+	
