0fb8ec658e52735ff75a4d77bf99b7d480b95de8
Vicilius <viniciusmachado19@gmail.com>
2021-06-08 16:34:09 -0300

    a

--- Explosions/Codigos/Control.gd
+++ Explosions/Codigos/Control.gd
@@ -19,7 +19,11 @@ var dialogos = {
 			"É meu ultimo més trabalhando aqui e eles resolvem meter uma BOMBA!"
 		],
 	"Cogumelo": [
-			"Merdaaaaaaaaa!!!!"
+			"Uma bomba?",
+			"Não esperava que a vida de todos daqui seria terminada por causa de uma bomba...",
+			"É uma pena, mas se caso alguém tenha a capacidade de desarmar isso tem um alicate em um dos armários nos escritórios....",
+			"Mas dúvido que alguem vai fazer isso."
+			
 		],
 	"Golem": [
 			"Eae, eu sou o zelador daqui!",
@@ -32,17 +36,39 @@ var dialogos = {
 		],
 	"Frank": [
 			"Eu sou o Frank, sou segurança aqui...",
-			"Mano! Essa música é muito boa!"
+			"Mano! Essa música é muito boa!",
+			"Ah tem uma bomba aqui?",
+			"Não me importo enquanto essa música tiver tocando eu vou dançar até minha morte"
+			
 	],
 	"Vampira": [
-			"Hmmm, essa noticia da bomba me dxou tão... empolgada!"
+			"Hmmm, essa noticia da bomba me dxou tão... empolgada!",
+			"Só de imaginar todos nós explodindo em pedaços me excita BASTANTE!",
+			"Você quer desarmar essa bomba gatinho?",
+			"Talvéz o terrorista tenha guardado a tal bomba em alguns dos banheiros, mas a porta ta emperrada então é dificil de entrar la",
+			"Se pelo menos alguém consiguisse arrombar de alguma forma.❤️♡♡♡♡♡",
+			"*ela se contorce de excitação*"
 	],
 	"Fantasma": [
 			"Que?... Bomba?...",
-			"Não ligo pra isso... Meu trabalho é mais importante..."
+			"Não ligo pra isso... Meu trabalho é mais importante...",
+			"Oque ainda ta me encarando? vai fazer outra coisa moleque, procura em algumas caixas e armários qualquer coisa ai",
+			"Deve ter algo utíl pra desarmar a tal bomba",
+			"Agora me deixe terminar meu relatório, contas não se pagam sozinhas."
 	],
 	"Lapis": [
-			"*ENCARA*"
+			"*ENCARA*",
+			"*ENCARA MAIS AINDA*",
+			"OQUE VOCÊ QUER SEU MALDITO",
+			"NÃO TA VENDO QUE EU SOU UM LÁPIS?",
+			"SIM UM MALDITO LÁPIS",
+			"MINHA FUNÇÃO É FICAR PERTO DESSA ESTATUA ATÉ ESSA FESTA TERMINAR, E NÃO FALAR NADA",
+			"OQUE UMA BOMBA?!",
+			"NÃO É POSSIVEL QUE EU MORRERREI INTERPRETANDO UM MALDITO LÁPIS",
+			"EU ME FORMEI EM TEATRO PARA SEGUIR O PAPEL DE UM GRANDE ATOR",
+			"MAS NINGUEM GOSTA DA MINHA MAGNIFICA ATUAÇÃO....",
+			"*ele começa a contar sobre sua vida e seus problemas, acho que é melhor deixa-lo ai*"
+			
 	],
 	"Porta": [
 			"Está trancada!"
